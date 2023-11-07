bc32bf436a54a6716771fe501b07e2c39efd7cbb
Vasundara <vjaya012@uottawa.ca>
2023-11-07 15:32:34 -0500

    Updated code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -10,23 +11,34 @@ app.use(express.json());
 // Get MongoDB URI from the environment variables
 const mongoURI = process.env.MONGODB_URI;
 
-// Connect to MongoDB using the MongoDB client
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+let dbClient;
+
+async function connectToMongoDB() {
+  if (!dbClient) {
+    dbClient = new MongoClient(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  try {
+    if (!dbClient.isConnected()) {
+      await dbClient.connect();
+      console.log('Connected to MongoDB');
+    }
+    return dbClient;
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    throw err;
+  }
+}
 
 // Define a route to retrieve files by collection name and filename
 app.get('/files/:collection/:filename', async (req, res) => {
-  await client.connect(); // Connect to MongoDB
-  const db = client.db("htdata");
   const { collection, filename } = req.params;
-
-  // Use decodeURIComponent to correctly handle special characters in the filename
   const decodedFilename = decodeURIComponent(filename);
 
-  // Define a map to validate and map the 'type' parameter to collection names
+  // Define a map to validate and map the 'collection' parameter to collection names
   const typeToCollectionMap = {
     bloodtest: 'Bloodtest_Report',
     mrispine: 'MRI_Spine',
@@ -37,33 +49,27 @@ app.get('/files/:collection/:filename', async (req, res) => {
     medicalhistory: 'Medical_History',
   };
 
-  // Check if the 'type' parameter is valid
-  if (!typeToCollectionMap.hasOwnProperty(collection)) {
-    res.status(400).json({ error: 'Invalid file type' });
-    return;
+  const collectionName = typeToCollectionMap[collection];
+
+  if (!collectionName) {
+    return res.status(400).json({ error: 'Invalid file type' });
   }
 
-  // Use the collection name directly for file retrieval
-  const fileModel = mongoose.model(typeToCollectionMap[collection], new mongoose.Schema({
-    originalname: String,
-    data: Buffer,
-    mimetype: String,
-  }));
-
   try {
-    const file = await fileModel.findOne({ originalname: decodedFilename });
+    const client = await connectToMongoDB();
+    const db = client.db("htdata");
+
+    const file = await db.collection(collectionName).findOne({ originalname: decodedFilename });
 
     if (!file) {
-      res.status(404).json({ error: 'File not found' });
-      return;
+      return res.status(404).json({ error: 'File not found' });
     }
 
     res.setHeader('Content-Type', file.mimetype);
     res.send(file.data);
   } catch (error) {
+    console.error('Error handling file request:', error);
     res.status(500).json({ error: 'Internal server error' });
-  } finally {
-    await client.close(); // Close the MongoDB connection
   }
 });
 
@@ -75,6 +81,6 @@ app.get('/', (req, res) => {
 
 // Get the port from the environment variables or use 3000 as a default
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, () => { 
   console.log(`Server is running on port ${port}`);
 });
